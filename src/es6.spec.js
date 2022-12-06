62dff8fc814fb8fdae28758fb7ab7d6477a58904
Eugene <zarov.gcg@gmail.com>
2022-12-06 17:44:32 +0400

    features

--- src/es6.spec.js
+++ src/es6.spec.js
@@ -43,5 +43,28 @@ describe('es6', () => {
             // TODO
             assert.strictEqual(!!dic, true);
         });
+        it ('вставка некорректных пар возвращает undefined', () => {
+            const dic = new core.Dictionary();
+            assert.strictEqual(dic.set("sex", undefined), undefined);
+            assert.strictEqual(dic.set(undefined, "sex"), undefined);
+            assert.strictEqual(dic.set("sex", null), undefined);
+            assert.strictEqual(dic.set(null, "sex"), undefined);
+            assert.strictEqual(dic.set([1, 2, 3], "sex"), undefined);
+            assert.strictEqual(dic.set([1, 2, 3], [1, 2, 3]), undefined);
+            assert.strictEqual(dic.set("sex", Symbol("symbol")), undefined);
+            
+        });
+
+        it ('проверка вставки и получения корректных данных', () => {
+            const dic = new core.Dictionary();
+            assert.strictEqual(dic.get("sex"), undefined);
+            dic.set("sex", "ssau");
+            assert.strictEqual(dic.get("sex"), "ssau");
+            assert.strictEqual(dic.get("sex"), "ssau");
+            dic.set("word1", "word2");
+            dic.set("word2", "word3");
+            assert.strictEqual(dic.get("word1"), "word2");
+            assert.strictEqual(dic.get("word2"), "word3");
+        });
     });
 });
--- src/oop.js
+++ src/oop.js
@@ -3,7 +3,17 @@
  * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
  * Со звездочкой: реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
  */
-class Point {}
+class Point {
+    constructor(x = 0, y = 0)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    length(){
+        return Math.sqrt(x**2 + y**2);
+    }
+
+}
 
 /**
  * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
@@ -11,7 +21,13 @@ class Point {}
  * Реализовать статический метод, который возвращает расстояние между Point3D.
  */
 class Point3D extends Point {
-    static vectorLength(a, b) {}
+    constructor(x, y, z = 0){
+        super(x,y);
+        this.z = z;
+    }
+    static vectorLength(a, b) {
+        return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2);
+    }
 }
 
 /**
@@ -20,7 +36,15 @@ class Point3D extends Point {
  * Со звездочкой: написать тесты методы класса (oop.spec.js)
  */
 class Queue {
-
+    constructor(array = []){
+        this.array = array;
+     }
+    enqueue(element){
+        this.array.push(element);
+    }
+    dequeue(){
+        return this.array.length === 0 ? "There is no elements to get" : this.array.shift();
+    }
 }
 
 module.exports = {
